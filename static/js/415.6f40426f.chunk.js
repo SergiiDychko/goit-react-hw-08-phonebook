2359eaf00991b09f21e0939092bf4434c4ebc639
Sergii Dychko <110810294+SergiiDychko@users.noreply.github.com>
2022-12-18 23:42:31 +0200

    Updates

---
+++ static/js/415.6f40426f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[415],{5415:function(e,o,n){n.r(o);var t=n(3329);o.default=function(){return(0,t.jsx)("h1",{children:"Welcome to Phonebook App"})}}}]);
+//# sourceMappingURL=415.6f40426f.chunk.js.map
